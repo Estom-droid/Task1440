c647505c7feace4469403ab9a936bb77d916ee5d
Estom-droid <i.antsupov@inbox.ru>
2025-10-19 17:19:14 +0300

    Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -4,11 +4,7 @@ def path(triangle):
     
     for row in range(len(triangle) - 2, -1, -1):
         for col in range(len(triangle[row])):
-
-            triangle[row][col] += min(
-                triangle[row + 1][col], 
-                triangle[row + 1][col + 1]
-            )
+            triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
     
     return triangle[0][0]
 #Задайте Треугольник  
